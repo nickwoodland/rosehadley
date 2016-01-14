38ab8ba582721c742e823964b6eca735bc2e23ac
nickwoodland <nickwoodland@gmail.com>
2016-01-14 17:12:06 +0000

    bad at this. restore foundationpress to former glory and roll in my changes cause i buggered it right up

--- wp-content/themes/FoundationPress/functions.php
+++ wp-content/themes/FoundationPress/functions.php
@@ -1,70 +1,53 @@
 <?php
-// Register Custom Post Type
-function rh_jobs_cpt() {
+/**
+ * Author: Ole Fredrik Lie
+ * URL: http://olefredrik.com
+ *
+ * FoundationPress functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
+ */
 
-	$labels = array(
-		'name'                  => _x( 'jobs', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Job', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Jobs', 'text_domain' ),
-		'name_admin_bar'        => __( 'Jobs', 'text_domain' ),
-		'archives'              => __( 'Job Archives', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-		'all_items'             => __( 'All Items', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Item', 'text_domain' ),
-		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-		'update_item'           => __( 'Update Item', 'text_domain' ),
-		'view_item'             => __( 'View Item', 'text_domain' ),
-		'search_items'          => __( 'Search Item', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
-	);
-	$args = array(
-		'label'                 => __( 'Job', 'text_domain' ),
-		'description'           => __( 'Job post type', 'text_domain' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
-	);
-	register_post_type( 'jobs', $args );
+/** Various clean up functions */
+require_once( 'library/cleanup.php' );
 
-}
-add_action( 'init', 'rh_jobs_cpt', 0 );
+/** Required for Foundation to work properly */
+require_once( 'library/foundation.php' );
 
-function jobs_taxonomy() {
-    register_taxonomy(
-        'job_types',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
-        'jobs',        //post type name
-        array(
-            'hierarchical' => true,
-            'label' => 'Job Types',  //Display name
-            'query_var' => true,
-            'rewrite' => array(
-                //'slug' => 'themes', // This controls the base slug that will display before each term
-                'with_front' => true // Don't display the category base before
-            )
-        )
-    );
-}
-add_action( 'init', 'jobs_taxonomy');
+/** Register all navigation menus */
+require_once( 'library/navigation.php' );
+
+/** Add menu walkers for top-bar and off-canvas */
+require_once( 'library/menu-walkers.php' );
+
+/** Create widget areas in sidebar and footer */
+require_once( 'library/widget-areas.php' );
+
+/** Return entry meta information for posts */
+require_once( 'library/entry-meta.php' );
+
+/** Enqueue scripts */
+require_once( 'library/enqueue-scripts.php' );
+
+/** Add theme support */
+require_once( 'library/theme-support.php' );
+
+/** Add Nav Options to Customer */
+require_once( 'library/custom-nav.php' );
+
+/** Change WP's sticky post class */
+require_once( 'library/sticky-posts.php' );
+
+/** If your site requires protocol relative url's for theme assets, uncomment the line below */
+// require_once( 'library/protocol-relative-theme-assets.php' );
+require_once( 'library/cpt-jobs.php' );
+
+require_once( 'library/cmbs.php' );
+?>
